ef1599ac32b06c5ba3168eebd2669393981f5a3c
Estefanno <estefanno.pimenta@gmail.com>
2025-09-16 11:11:10 -0300

    update 3-logica.js & create 4-logica.js

--- 3-logica.js
+++ 3-logica.js
@@ -1,18 +1,23 @@
 
-let area = prompt('Dentro da tecnologia, qual área você quer seguir: Frontend ou Backend?')
+let area = prompt("Dentro da tecnologia, qual área você quer seguir 'Front-end' ou 'Back-end?': Digite o nome da área: ")
 
-if(area == "Frontend") {
-    prompt('Muito legal! Dentro do Frontend voce quer usar qual framework: Vue ou React?')
+if(area === "Front-end") {
+    prompt('Muito legal! Dentro do Front-end voce quer usar qual framework: Vue ou React?')
+} else if(area === "Back-end") {
+    prompt('Muito legal! Dentro do Back-end voce quer usar qual linguagem: C# ou Java?')
 } else {
-    prompt('Muito legal! Dentro do Backend voce quer usar qual linguagem: C# ou Java?')
+    alert('Área inválida. Por favor, escolha entre "Front-end" ou "Back-end".')
 }
 
-let especializacao = prompt('Voce quer seguir se especializando na área ou tornar-se um Fullstack?')
 
-if(especializacao == "especializando") {
+let especializacao = prompt(' Digite 1 se você quer seguir se especializando na área escolhida ou 2 para tornar-se um Fullstack: ')
+
+if(especializacao == 1) {
     alert('Muito legal! Especialização vai te tornar um profissional muito requisitado!')
-} else {
+} else if (especializacao == 2) {
     alert('Muito legal! Fullstack é o futuro!')
+} else {
+    alert('Opção inválida. Por favor, digite 1 ou 2.')
 }
 
 let tecnologias = []
